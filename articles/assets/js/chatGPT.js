3238d8b106e41595e3649cd3386d4696c4e778fc
klemac <klee3663@usc.edu>
2023-03-18 14:29:15 -0700

    fa

--- articles/assets/js/chatGPT.js
+++ articles/assets/js/chatGPT.js
@@ -1,19 +1,16 @@
 // Load the Google Identity Services (GIS) library
 function initializeSignInButton() {
-  gapi.load('signin2', renderSignInButton);
+  gapi.load('signin', renderSignInButton);
 }
 
 // Render the Google Sign-In button
 function renderSignInButton() {
-  gapi.signin2.render('google-signin-btn', {
-    'width': 250,
-    'height': 50,
-    'longtitle': true,
-    'theme': 'dark',
-    'onsuccess': onSignIn,
-    'onfailure': function() {
-      console.log('Sign-in error');
-    }
+  gapi.signin.render('google-signin-btn', {
+    'client_id': '801961296119-s4u306t6rggorr92gtq8pc54uuhalirq.apps.googleusercontent.com',
+    'callback': onSignIn,
+    'scope': 'profile email',
+    'ux_mode': 'popup',
+    'theme': 'dark'
   });
 }
 
@@ -30,7 +27,6 @@ async function onSignIn(googleUser) {
   $('#welcome-message').text(`Welcome, ${firstName}!`);
 }
 
-
 // Function to handle the sign-out process
 function signOut() {
   const auth2 = gapi.auth2.getAuthInstance();
@@ -41,23 +37,32 @@ function signOut() {
   });
 }
 
-async function handleCredentialResponse(response) {
-  try {
-    const token = response.credential;
-    const userInfo = await fetch('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=' + token);
-    const userData = await userInfo.json();
-
-    const userId = userData.email;
-    const firstName = userData.given_name;
-
-    $('#user-id').val(userId);
-    $('#welcome-message').text('Welcome, ' + firstName + '!');
-  } catch (error) {
-    console.error('Error handling credential response:', error);
-  }
+// Initialize the Google Sign-In button
+function initGoogleAuth() {
+  gapi.load('auth2', () => {
+    gapi.auth2.init({
+      client_id: '801961296119-s4u306t6rggorr92gtq8pc54uuhalirq.apps.googleusercontent.com',
+      scope: 'profile email',
+    }).then(() => {
+      attachSignInHandler();
+    });
+  });
 }
 
+function attachSignInHandler() {
+  const signInButton = document.getElementById('google-signin-btn');
+  signInButton.addEventListener('click', () => {
+    signIn();
+  });
+}
 
-window.onload = initializeSignInButton;
+function signIn() {
+  const auth2 = gapi.auth2.getAuthInstance();
+  auth2.signIn()
+    .then(onSignIn)
+    .catch(onSignInFailure);
+}
 
-initializeSignInButton();
+function onSignInFailure(error) {
+  console.error('Error during sign-in:', error);
+}
