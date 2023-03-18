b7679e2702e80c3c1666571e785ba32c50e5b5d6
klemac <klee3663@usc.edu>
2023-03-18 15:51:29 -0700

    faf

--- articles/signin.js
+++ articles/signin.js
@@ -5,5 +5,13 @@ function onSignIn(googleUser) {
     $("profile").val("src". profile.getImageUrl());
     $("data").css("display", "block");
     $(".g-signin2").css("display", "none");
-    
+
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        $("data").css("display", "none");
+        $(".g-signin2").css("display", "block");
+    });
 }
